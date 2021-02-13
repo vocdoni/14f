32150402b623ebcf96c354b57f6f4af1400e8e82
Dario Castañé <d@rio.hn>
2021-02-13 23:09:41 +0100

    Improve mobile layout

--- pages/staging/components/statistics_form.tsx
+++ pages/staging/components/statistics_form.tsx
@@ -1,12 +1,12 @@
 const StatisticsForm = ({ onCodeUpdate, onResidencyUpdate }) => {
     return (
-        <div className="mb-6 text-sm text-gray-600">
+        <div className="mb-6 text-xs text-gray-600 lg:text-sm">
             <p className="mb-2">
                 Abans de triar, i de forma voluntària, pots respondre aquestes
                 preguntes. Recollim aquestes dades només amb finalitats
                 estadístiques i no estan vinculades al teu vot.
             </p>
-            <div className="grid grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 gap-4 lg:gap-6 lg:grid-cols-4">
                 <div className="col-auto">
                     <label htmlFor="postal-code" className="font-bold">
                         Codi postal
@@ -22,34 +22,46 @@ const StatisticsForm = ({ onCodeUpdate, onResidencyUpdate }) => {
                 <div className="col-span-3">
                     <div className="my-auto">
                         <p className="font-bold">Sóc resident a</p>
-                        <div className="mt-1">
-                            <input
-                                id="dni"
-                                type="radio"
-                                name="residency"
-                                className="mr-1"
-                                value="dni"
-                                onChange={(e) => onResidencyUpdate(e.currentTarget.value)}
-                            />
-                            <label htmlFor="dni">Catalunya</label>
-                            <input
-                                id="nie"
-                                type="radio"
-                                className="ml-3 mr-1"
-                                name="residency"
-                                value="nie"
-                                onChange={(e) => onResidencyUpdate(e.currentTarget.value)}
-                            />
-                            <label htmlFor="nie">Catalunya amb NIE</label>
-                            <input
-                                id="foreign"
-                                type="radio"
-                                className="ml-3 mr-1"
-                                name="residency"
-                                value="foreign"
-                                onChange={(e) => onResidencyUpdate(e.currentTarget.value)}
-                            />
-                            <label htmlFor="foreign">l'estranger</label>
+                        <div className="grid grid-cols-1 mt-1 lg:block">
+                            <div className="lg:inline">
+                                <input
+                                    id="dni"
+                                    type="radio"
+                                    name="residency"
+                                    className="mr-1"
+                                    value="dni"
+                                    onChange={(e) =>
+                                        onResidencyUpdate(e.currentTarget.value)
+                                    }
+                                />
+                                <label htmlFor="dni">Catalunya</label>
+                            </div>
+                            <div className="lg:inline">
+                                <input
+                                    id="nie"
+                                    type="radio"
+                                    className="mr-1 lg:ml-3"
+                                    name="residency"
+                                    value="nie"
+                                    onChange={(e) =>
+                                        onResidencyUpdate(e.currentTarget.value)
+                                    }
+                                />
+                                <label htmlFor="nie">Cat. amb NIE</label>
+                            </div>
+                            <div className="lg:inline">
+                                <input
+                                    id="foreign"
+                                    type="radio"
+                                    className="mr-1 lg:ml-3"
+                                    name="residency"
+                                    value="foreign"
+                                    onChange={(e) =>
+                                        onResidencyUpdate(e.currentTarget.value)
+                                    }
+                                />
+                                <label htmlFor="foreign">l'estranger</label>
+                            </div>
                         </div>
                     </div>
                 </div>
