9601fe17d61011f23e6665c5d360d02b2e4ccb4f
Dario Castañé <i@dario.im>
2021-02-13 16:16:23 +0100

    Add GDPR acceptance step (#15)

--- pages/staging/components/region_selector.tsx
+++ pages/staging/components/region_selector.tsx
@@ -12,10 +12,10 @@ const RegionSelector = ({ onSelect, onBackNavigation }) => {
         <Container>
             <header>
                 <div className="mb-2 font-light text-center mt-7 text-vocdoni text-md lg:text-lg">
-                    Pas 01 de 02
+                    Pas 02 de 03
                 </div>
                 <h1 className="mb-5 text-3xl font-bold leading-none tracking-tight text-center text-gray-900 lg:text-4xl sm:mb-7">
-                    Abans, quina és la teva circumscripció electoral?
+                    I quina és la teva circumscripció electoral?
                 </h1>
             </header>
             <div className="grid grid-cols-1 gap-4 text-2xl mb-14 sm:mb-20 xl:mb-8">
