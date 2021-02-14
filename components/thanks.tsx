480b43b060dfe45e2cd40360b1831a7204f6c37f
Dario Castañé <i@dario.im>
2021-02-14 11:24:04 +0100

    Add warning to avoid vote and tweets correlation (#26)

--- components/thanks.tsx
+++ components/thanks.tsx
@@ -38,12 +38,21 @@ const Thanks = ({ nullifier }) => {
                 <p className="p-4 mb-3 font-mono text-xs font-medium text-center break-all border border-blue-100 lg:text-sm rounded-xl">
                     {nullifier?.replace(/^0x/, "")}
                 </p>
-                <p className="mb-6 font-medium text-vocdoni">
+                <p className="mb-3 font-medium text-vocdoni">
                     Amb aquest identificant podràs validar que la teva opció ha
                     estat correctament comptada en qualsevol moment.{" "}
                     <strong>No el comparteixis!</strong> Els resultats es faran
                     públics tan bon punt es tanquin els col·legis electorals.
                 </p>
+                <div className="px-4 py-4 mb-6 text-sm text-yellow-800 bg-yellow-100 border border-transparent lg:text-md rounded-xl">
+                    Et recomanem que esperis un temps prudencial (com a mínim
+                    uns minuts) abans de compartir la teva experiència a les
+                    xarxes socials. Més informació a les{" "}
+                    <a className="underline" href="https://www.notion.so/Preguntes-freq-ents-14Fruites-6c04e6d2beb14ba282dc2b10b0feb59a">
+                        FAQs
+                    </a>
+                    .
+                </div>
                 <div>
                     <h2 className="mb-2">
                         Comparteix la iniciativa a xarxes socials!
